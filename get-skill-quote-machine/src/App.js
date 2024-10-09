4e95d24683e0b208e749443da8f1793a4cdf2a49
moonprincelol <patrickwang2003@gmail.com>
2024-10-09 16:05:42 +0800

    change to scss

--- get-skill-quote-machine/src/App.js
+++ get-skill-quote-machine/src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './App.scss';
 
 function App() {
+  
+  let quote = "I'm a quote!";
+  let author = "I'm an author!";
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <p>{quote}</p>
+      <p>- {author}</p>
       </header>
     </div>
   );
